b0efd9a80773ca5d8860f133be0b8da3fe5d7852
Matthew Ray <matthewray.pgh@live.com>
2016-09-01 20:53:37 -0400

    schedule obj updates... draft with nested arrays

--- manageSchedules.Client/app/schedule/scheduleCtrl.js
+++ manageSchedules.Client/app/schedule/scheduleCtrl.js
@@ -50,13 +50,42 @@
                 'id': 4,
                 'name': 'Seymour Butts',
                 'ttlShifts': 3,
-                'schedule': { 'Sun': { 'AM': 0, 'PM': 0 }, 'Mon': { 'AM': 0, 'PM': 0 }, 'Tue': { 'AM': 0, 'PM': 0 }, 'Wed': { 'AM': 0, 'PM': 0 }, 'Thu': { 'AM': 0, 'PM': 0 }, 'Fri': { 'AM': 0, 'PM': 0 }, 'Sat': { 'AM': 0, 'PM': 0 } }
+                'day': [{
+                    'id': 0, 'name':'Sun', 'shift': [{
+                        'id': 0, 'name': 'AM', 'status': 0,
+                        'id': 1, 'name': 'PM', 'status': 0
+                    }],
+                    'id': 1, 'name':'Mon', 'shift': {
+                        'id': 0, 'name': 'AM', 'status': 0,
+                        'id': 1, 'name': 'PM', 'status': 0
+                    },
+                    'id': 2, 'name':'Tue', 'shift': {
+                        'id': 0, 'name': 'AM', 'status': 0,
+                        'id': 1, 'name': 'PM', 'status': 0
+                    },
+                    'id': 3, 'name':'Wed', 'shift': {
+                        'id': 0, 'name': 'AM', 'status': 0,
+                        'id': 1, 'name': 'PM', 'status': 0
+                    },
+                    'id': 4, 'name':'Thu', 'shift': {
+                        'id': 0, 'name': 'AM', 'status': 0,
+                        'id': 1, 'name': 'PM', 'status': 0
+                    },
+                    'id': 5, 'name':'Fri', 'shift': {
+                        'id': 0, 'name': 'AM', 'status': 0,
+                        'id': 1, 'name': 'PM', 'status': 0
+                    },
+                    'id': 6, 'name':'Sat', 'shift': {
+                        'id': 0, 'name': 'AM', 'status': 0,
+                        'id': 1, 'name': 'PM', 'status': 0
+                    }
+                }]
             }
         ];
 
         vm.updSchedule = updSchedule;
 
-        function updSchedule(i, val) {
+        function updSchedule(i, emp, day, shift) {
             console.log('employee', vm.employeeObj[i].name);
             console.log('schedule', vm.employeeObj[i].schedule);
         }
