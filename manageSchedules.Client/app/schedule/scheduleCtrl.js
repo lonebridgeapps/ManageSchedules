eb4f269cd0b13002e9949e232df52db959ee9851
lonebridgeapps <lonebridgeapps@gmail.com>
2016-09-02 17:52:24 -0400

    t

--- manageSchedules.Client/app/schedule/scheduleCtrl.js
+++ manageSchedules.Client/app/schedule/scheduleCtrl.js
@@ -46,39 +46,61 @@
                 'ttlShifts': 5,
                 'schedule': { 'Sun': { 'AM': 0, 'PM': 0 }, 'Mon': { 'AM': 0, 'PM': 0 }, 'Tue': { 'AM': 0, 'PM': 0 }, 'Wed': { 'AM': 0, 'PM': 0 }, 'Thu': { 'AM': 0, 'PM': 0 }, 'Fri': { 'AM': 0, 'PM': 0 }, 'Sat': { 'AM': 0, 'PM': 0 } }
             },
-            {
-                'id': 4,
-                'name': 'Seymour Butts',
-                'ttlShifts': 3,
-                'day': [{
-                    'id': 0, 'name':'Sun', 'shift': [{
-                        'id': 0, 'name': 'AM', 'status': 0,
-                        'id': 1, 'name': 'PM', 'status': 0
-                    }],
-                    'id': 1, 'name':'Mon', 'shift': {
-                        'id': 0, 'name': 'AM', 'status': 0,
-                        'id': 1, 'name': 'PM', 'status': 0
-                    },
-                    'id': 2, 'name':'Tue', 'shift': {
-                        'id': 0, 'name': 'AM', 'status': 0,
-                        'id': 1, 'name': 'PM', 'status': 0
-                    },
-                    'id': 3, 'name':'Wed', 'shift': {
-                        'id': 0, 'name': 'AM', 'status': 0,
-                        'id': 1, 'name': 'PM', 'status': 0
-                    },
-                    'id': 4, 'name':'Thu', 'shift': {
-                        'id': 0, 'name': 'AM', 'status': 0,
-                        'id': 1, 'name': 'PM', 'status': 0
-                    },
-                    'id': 5, 'name':'Fri', 'shift': {
-                        'id': 0, 'name': 'AM', 'status': 0,
-                        'id': 1, 'name': 'PM', 'status': 0
-                    },
-                    'id': 6, 'name':'Sat', 'shift': {
-                        'id': 0, 'name': 'AM', 'status': 0,
-                        'id': 1, 'name': 'PM', 'status': 0
-                    }
+        {
+            'id': 4,
+            'name': 'Seymour Butts',
+            'ttlShifts': 3,
+            'day': [
+                {
+                    'id': 0,
+                    'name': 'Sun',
+                    'shift': [
+                        { 'id': 0, 'name': 'AM', 'status': 0 },
+                        { 'id': 1, 'name': 'PM', 'status': 0 }
+                    ]
+                },
+                {
+                    'id': 1,
+                    'name': 'Mon',
+                    'shift': [
+                        { 'id': 0, 'name': 'AM', 'status': 0 },
+                        { 'id': 1, 'name': 'PM', 'status': 0 }
+                    ]
+                }, {
+                    'id': 2,
+                    'name': 'Tue',
+                    'shift': [
+                        { 'id': 0, 'name': 'AM', 'status': 0 },
+                        { 'id': 1, 'name': 'PM', 'status': 0 }
+                    ]
+                }, {
+                    'id': 3,
+                    'name': 'Wed',
+                    'shift': [
+                        { 'id': 0, 'name': 'AM', 'status': 0 },
+                        { 'id': 1, 'name': 'PM', 'status': 0 }
+                    ]
+                }, {
+                    'id': 4,
+                    'name': 'Thu',
+                    'shift': [
+                        { 'id': 0, 'name': 'AM', 'status': 0 },
+                        { 'id': 1, 'name': 'PM', 'status': 0 }
+                    ]
+                }, {
+                    'id': 5,
+                    'name': 'Fri',
+                    'shift': [
+                        { 'id': 0, 'name': 'AM', 'status': 0 },
+                        { 'id': 1, 'name': 'PM', 'status': 0 }
+                    ]
+                }, {
+                    'id': 6,
+                    'name': 'Sat',
+                    'shift': [
+                        { 'id': 0, 'name': 'AM', 'status': 0 },
+                        { 'id': 1, 'name': 'PM', 'status': 0 }
+                    ]
                 }]
             }
         ];
